17ace38c19a8285dfeb9468882ad26180eea4a46
patricklafrance <pa.lafrance@gmail.com>
2023-05-10 22:46:55 -0400

    Fixed a few typos

--- packages/react-router/src/runtime.ts
+++ packages/react-router/src/runtime.ts
@@ -5,26 +5,26 @@ import type { RootRoute } from "./routeRegistry.ts";
 import { RouteRegistry } from "./routeRegistry.ts";
 
 export class Runtime extends AbstractRuntime<RootRoute, RootNavigationItem> {
-    protected _routeRegistry = new RouteRegistry();
-    protected _navigationItemRegistry = new NavigationItemRegistry();
+    readonly #routeRegistry = new RouteRegistry();
+    readonly #navigationItemRegistry = new NavigationItemRegistry();
 
     registerRoutes(routes: RootRoute[]) {
-        this._routeRegistry.add(routes);
+        this.#routeRegistry.add(routes);
 
         this._logger.debug(`[squide] The following route${routes.length !== 1 ? "s" : ""} has been registered.`, routes);
     }
 
     get routes() {
-        return this._routeRegistry.routes;
+        return this.#routeRegistry.routes;
     }
 
     registerNavigationItems(navigationItems: RootNavigationItem[]) {
-        this._navigationItemRegistry.add(navigationItems);
+        this.#navigationItemRegistry.add(navigationItems);
 
         this._logger.debug(`[squide] The following navigation item${navigationItems.length !== 1 ? "s" : ""} has been registered.`, navigationItems);
     }
 
     get navigationItems() {
-        return this._navigationItemRegistry.items;
+        return this.#navigationItemRegistry.items;
     }
 }

166f607aa3ba98681117dfb932bf281cfa08d826
patricklafrance <pa.lafrance@gmail.com>
2023-05-17 17:10:38 -0400

    Try to fix tests on CI

--- packages/react-router/jest.config.ts
+++ packages/react-router/jest.config.ts
@@ -2,11 +2,15 @@ import { type Config } from "jest";
 import { config as swcConfig } from "./swc.jest.ts";
 
 const config: Config = {
+    // rootDir: "../../../",
     testEnvironment: "jsdom",
     testRegex: "/tests/*/.*\\.test\\.ts?(x)$",
     testPathIgnorePatterns: ["/node_modules/", "/dist/"],
     transform: {
         "^.+\\.(js|ts|tsx)$": ["@swc/jest", swcConfig as Record<string, unknown>]
+    },
+    moduleNameMapper: {
+        "@squide/core(.*)$": "../../../packages/core/src/$1"
     },
     verbose: true
 };

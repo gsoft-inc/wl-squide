973621dd2172c39481c86cf4f85c3838eb82f39a
Patrick Lafrance <patricklafrance@users.noreply.github.com>
2023-05-25 10:55:23 -0400

    Improved jest setup (#17)

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # wl-squide
+
+## Rules of engagements
+
+-> 2 biggest problems with frontend federated apps are:
+    - How to offer a cohesive experience which doesn't feels "modular"
+    - How to not load the same large dependencies twice when switching between "modules"
+
+-> Webpack Module Federation helps with that but there's a cost
+    - Dependencies update is trickier
+    - Requires to configure shared dependencies
+    - Library must be backward compatible until every "modules" updated to the new version
--- packages/webpack-module-federation/jest.config.ts
+++ packages/webpack-module-federation/jest.config.ts
@@ -1,17 +1,18 @@
 import type { Config } from "jest";
+import { compilerOptions } from "./tsconfig.json";
+import { pathsToModuleNameMapper } from "ts-jest";
 import { config as swcConfig } from "./swc.jest.ts";
 
 const config: Config = {
     testEnvironment: "node",
-    testRegex: "/tests/*/.*\\.test\\.ts$",
-    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
     transform: {
         "^.+\\.(js|ts)$": ["@swc/jest", swcConfig as Record<string, unknown>]
     },
     moduleNameMapper: {
-        "@squide/core(.*)$": "../../../packages/core/src/$1"
-    },
-    verbose: true
+        ...pathsToModuleNameMapper(compilerOptions.paths, {
+            prefix: "<rootDir>"
+        })
+    }
 };
 
 export default config;

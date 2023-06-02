8773b08f77bec056678143c01864430371458d77
patricklafrance <pa.lafrance@gmail.com>
2023-06-02 10:10:27 -0400

    Code review fixes

--- packages/core/src/messaging/useEventBusDispatcher.ts
+++ packages/core/src/messaging/useEventBusDispatcher.ts
@@ -1,10 +1,11 @@
 import type { EventName } from "./eventBus.ts";
+import { useCallback } from "react";
 import { useEventBus } from "../runtime/useEventBus.ts";
 
 export function useEventBusDispatcher<TEventNames extends EventName>() {
     const eventBus = useEventBus<TEventNames>();
 
-    return (eventName: TEventNames, data?: unknown) => {
+    return useCallback((eventName: TEventNames, data?: unknown) => {
         eventBus.dispatch(eventName, data);
-    };
+    }, [eventBus]);
 }

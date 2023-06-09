1b37a9fc795fe5e92f4c0cc9ca8adf1cb43c338e
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-06-09 11:58:23 -0400

    update dependencies

--- sample/local-module/src/Message.tsx
+++ sample/local-module/src/Message.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useCallback, useState } from "react";
+import { type ChangeEvent, useCallback, useState } from "react";
 
 import { Link } from "react-router-dom";
 import { useApplicationEventBusDispatcher } from "@sample/shared";

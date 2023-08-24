3b20b66c2b71c226b4807c6e402a1cf38e40f4a5
patricklafrance <pa.lafrance@gmail.com>
2023-08-23 22:11:04 -0400

    Try to fix build

--- packages/react-router/src/index.ts
+++ packages/react-router/src/index.ts
@@ -2,10 +2,7 @@ export * from "@squide/core";
 
 export * from "./runtime.ts";
 
-// export * from "./routeRegistry.ts";
-
-export { RouteRegistry } from "./routeRegistry.ts";
-export type { RootRoute, Route } from "./routeRegistry.ts";
+export * from "./routeRegistry.ts";
 
 export * from "./useHoistedRoutes.ts";
 export * from "./useRoutes.ts";
@@ -13,8 +10,4 @@ export * from "./useRoutes.ts";
 export * from "./navigationItemRegistry.ts";
 export * from "./useNavigationItems.ts";
 export * from "./useRenderedNavigationItems.tsx";
-
-// import type { RouteObject } from "react-router-dom";
-
-// export type { RouteObject };
 

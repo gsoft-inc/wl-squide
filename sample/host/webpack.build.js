2373bb426530bea2fbe03d04bb591e4238682e49
Patrick Lafrance <patricklafrance@users.noreply.github.com>
2023-09-06 14:43:42 -0400

    Update sample/host/webpack.build.js
    Co-authored-by: Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>

--- sample/host/webpack.build.js
+++ sample/host/webpack.build.js
@@ -3,7 +3,7 @@
 import { defineBuildHostConfig } from "@squide/webpack-module-federation/defineConfig.js";
 import { swcConfig } from "./swc.build.js";
 
-// The trailing / is very important, otherwise paths will ne be resolved correctly.
+// The trailing / is very important, otherwise paths will not be resolved correctly.
 const publicPath = process.env.NETLIFY === "true" ? "https://squide-host.netlify.app/" : "http://localhost:8080/";
 
 export default defineBuildHostConfig(swcConfig, "host", publicPath, {

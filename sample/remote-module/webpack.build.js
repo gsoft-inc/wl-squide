0be29d7409b5724f5187117be28b33240dcafb14
Patrick Lafrance <patricklafrance@users.noreply.github.com>
2023-09-06 14:43:33 -0400

    Update sample/remote-module/webpack.build.js
    Co-authored-by: Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>

--- sample/remote-module/webpack.build.js
+++ sample/remote-module/webpack.build.js
@@ -3,7 +3,7 @@
 import { defineBuildRemoteModuleConfig } from "@squide/webpack-module-federation/defineConfig.js";
 import { swcConfig } from "./swc.build.js";
 
-// The trailing / is very important, otherwise paths will ne be resolved correctly.
+// The trailing / is very important, otherwise paths will not be resolved correctly.
 const publicPath = process.env.NETLIFY === "true" ? "https://squide-remote-module.netlify.app/" : "http://localhost:8081/";
 
 export default defineBuildRemoteModuleConfig(swcConfig, "remote1", publicPath, {

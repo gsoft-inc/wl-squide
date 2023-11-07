1e1770797411151255a49277519f73e2eb90e565
patricklafrance <pa.lafrance@gmail.com>
2023-11-06 23:29:38 -0500

    Updated packages

--- samples/endpoints/host/swc.build.js
+++ samples/endpoints/host/swc.build.js
@@ -4,20 +4,4 @@ import { browserslistToSwc, defineBuildConfig } from "@workleap/swc-configs";
 
 const targets = browserslistToSwc();
 
-/**
- * Temporary transformer to enable loose mode until https://github.com/swc-project/swc/issues/8178 is fixed.
- * @typedef {import("@workleap/swc-configs").SwcConfig} SwcConfig
- * @param {SwcConfig} config
- * @returns {SwcConfig}
- */
-function temporaryEnablingLooseMode(config) {
-    if (config && config.jsc) {
-        config.jsc.loose = true;
-    }
-
-    return config;
-}
-
-export const swcConfig = defineBuildConfig(targets, {
-    transformers: [temporaryEnablingLooseMode]
-});
+export const swcConfig = defineBuildConfig(targets);

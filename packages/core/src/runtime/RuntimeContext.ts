599c9074afb813451ab31a1d96cd08240251743a
patricklafrance <pa.lafrance@gmail.com>
2023-12-04 23:31:06 -0500

    Fixed bad error message

--- packages/core/src/runtime/RuntimeContext.ts
+++ packages/core/src/runtime/RuntimeContext.ts
@@ -8,7 +8,7 @@ export function useRuntime() {
     const runtime = useContext(RuntimeContext);
 
     if (isNil(runtime)) {
-        throw new Error("[squide] The useRuntime function is called before a Runtime instance has been provided. Did you forgot to ");
+        throw new Error("[squide] The useRuntime function is called before a Runtime instance has been provided.");
     }
 
     return runtime;

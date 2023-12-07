11111f0fc1116571ca2e622ee5927376c00d68e0
patricklafrance <pa.lafrance@gmail.com>
2023-12-07 16:17:58 -0500

    Using useSyncExternalStore also for MSW

--- packages/msw/src/useIsMswStarted.ts
+++ packages/msw/src/useIsMswStarted.ts
@@ -1,37 +1,23 @@
 import { useLogger } from "@squide/core";
-import { useEffect, useState } from "react";
-import { isMswStarted } from "./setMswAsStarted.ts";
+import { useEffect, useSyncExternalStore } from "react";
+import { addMswStateChangedListener, isMswStarted, removeMswStateChangedListener } from "./mswState.ts";
 
-export interface UseIsMswStartedOptions {
-    // The interval is in milliseconds.
-    interval?: number;
+function subscribe(callback: () => void) {
+    addMswStateChangedListener(callback);
+
+    return () => removeMswStateChangedListener(callback);
 }
 
-export function useIsMswStarted(enabled: boolean, { interval = 10 }: UseIsMswStartedOptions = {}) {
+export function useIsMswStarted(enabled: boolean) {
+    const isStarted = useSyncExternalStore(subscribe, isMswStarted);
+
     const logger = useLogger();
 
-    // Using a state hook to force a rerender once MSW is started.
-    const [value, setIsStarted] = useState(!enabled);
-
-    // Perform a reload once MSW is started.
     useEffect(() => {
-        if (enabled) {
-            const intervalId = setInterval(() => {
-                if (isMswStarted()) {
-                    logger.debug("[squide] %cMSW is ready%c.", "color: white; background-color: green;", "");
-
-                    clearInterval(intervalId);
-                    setIsStarted(true);
-                }
-            }, interval);
-
-            return () => {
-                if (intervalId) {
-                    clearInterval(intervalId);
-                }
-            };
+        if (isStarted) {
+            logger.debug("[squide] %cMSW is ready%c.", "color: white; background-color: green;", "");
         }
-    }, [enabled, interval, logger]);
+    }, [isStarted, logger]);
 
-    return value;
+    return isStarted || !enabled;
 }

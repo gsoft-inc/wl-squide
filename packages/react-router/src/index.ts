43f6a27b5c92efae9f044e4b5f153fd18b2cc129
patricklafrance <pa.lafrance@gmail.com>
2024-01-18 14:11:52 -0500

    Removed dead code

--- packages/react-router/src/index.ts
+++ packages/react-router/src/index.ts
@@ -2,7 +2,7 @@ export * from "./navigationItemRegistry.ts";
 export * from "./outlets.ts";
 export * from "./reactRouterRuntime.ts";
 export * from "./routeRegistry.ts";
-export * from "./useIsRouteMatchProtected.ts";
+export * from "./useIsRouteProtected.ts";
 export * from "./useNavigationItems.ts";
 export * from "./useRenderedNavigationItems.tsx";
 export * from "./useRouteMatch.ts";

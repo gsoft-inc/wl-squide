4462559eb77cb820206971232e5aec904dcb853d
patricklafrance <pa.lafrance@gmail.com>
2024-02-13 15:17:16 -0500

    Adding a generic type for event bus payload

--- packages/core/src/messaging/useEventBusDispatcher.ts
+++ packages/core/src/messaging/useEventBusDispatcher.ts
@@ -2,10 +2,10 @@ import { useCallback } from "react";
 import { useEventBus } from "../runtime/useEventBus.ts";
 import type { EventName } from "./eventBus.ts";
 
-export function useEventBusDispatcher<TEventNames extends EventName>() {
-    const eventBus = useEventBus<TEventNames>();
+export function useEventBusDispatcher<TEventNames extends EventName, TPayload = unknown>() {
+    const eventBus = useEventBus<TEventNames, TPayload>();
 
-    return useCallback((eventName: TEventNames, data?: unknown) => {
-        eventBus.dispatch(eventName, data);
+    return useCallback((eventName: TEventNames, payload?: TPayload) => {
+        eventBus.dispatch(eventName, payload);
     }, [eventBus]);
 }
--- packages/core/src/runtime/useEventBus.ts
+++ packages/core/src/runtime/useEventBus.ts
@@ -2,8 +2,8 @@ import type { EventBus, EventName } from "../messaging/eventBus.ts";
 
 import { useRuntime } from "./RuntimeContext.ts";
 
-export function useEventBus<TEventNames extends EventName>() {
+export function useEventBus<TEventNames extends EventName, TPayload = unknown>() {
     const runtime = useRuntime();
 
-    return runtime.eventBus as unknown as EventBus<TEventNames>;
+    return runtime.eventBus as unknown as EventBus<TEventNames, TPayload>;
 }

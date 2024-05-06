aea1c6bbd5564fedfc98892c5e628549e57ae785
patricklafrance <pa.lafrance@gmail.com>
2024-05-06 11:43:25 -0400

    Added support for deploy basic

--- samples/basic/host/remotes.js
+++ samples/basic/host/remotes.js
@@ -1,17 +1,15 @@
 // @ts-check
 
-import { isNetlify } from "@basic/shared";
-
 /**
  * @typedef {import("@squide/firefly-webpack-configs").RemoteDefinition[]}
  */
 export const Remotes = [
     {
         name: "remote1",
-        url: isNetlify ? "https://squide-basic-remote-module.netlify.app" : "http://localhost:8081"
+        url: process.env.NETLIFY === "true" ? "https://squide-basic-remote-module.netlify.app" : "http://localhost:8081"
     },
     {
         name: "remote2",
-        url: isNetlify ? "https://squide-basic-another-remote-module.netlify.app" : "http://localhost:8082"
+        url: process.env.NETLIFY === "true" ? "https://squide-basic-another-remote-module.netlify.app" : "http://localhost:8082"
     }
 ];
--- samples/endpoints/host/remotes.js
+++ samples/endpoints/host/remotes.js
@@ -1,13 +1,11 @@
 // @ts-check
 
-import { isNetlify } from "@endpoints/shared";
-
 /**
  * @typedef {import("@squide/firefly-webpack-configs").RemoteDefinition[]}
  */
 export const Remotes = [
     {
         name: "remote1",
-        url: isNetlify ? "https://squide-endpoints-remote-module.netlify.app" : "http://localhost:8081"
+        url: process.env.NETLIFY === "true" ? "https://squide-endpoints-remote-module.netlify.app" : "http://localhost:8081"
     }
 ];

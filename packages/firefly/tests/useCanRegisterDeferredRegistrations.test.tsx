4b4abcc4a24baa8dbf2c49ecc8b7a84c9076e683
patricklafrance <pa.lafrance@gmail.com>
2024-07-29 22:05:04 -0400

    More stuff

---
+++ packages/firefly/tests/useCanRegisterDeferredRegistrations.test.tsx
@@ -0,0 +1,135 @@
+import { renderHook, type RenderHookOptions } from "@testing-library/react";
+import type { ReactNode } from "react";
+import { AppRouterStateContext } from "../src/AppRouterContext.ts";
+import type { AppRouterState } from "../src/AppRouterReducer.ts";
+import { useCanRegisterDeferredRegistrations } from "../src/useCanRegisterDeferredRegistrations.ts";
+
+function createDefaultAppRouterState(): AppRouterState {
+    return {
+        areModulesReady: false,
+        areModulesRegistered: false,
+        isActiveRouteProtected: false,
+        isMswReady: false,
+        isProtectedDataReady: false,
+        isPublicDataReady: false,
+        isUnauthorized: false,
+        waitForMsw: false,
+        waitForProtectedData: false,
+        waitForPublicData: false
+    };
+}
+
+function renderUseCanRegisterDeferredRegistrationsHook<TProps>(state: AppRouterState, additionalProps: RenderHookOptions<TProps> = {}) {
+    return renderHook(() => useCanRegisterDeferredRegistrations(), {
+        wrapper: ({ children }: { children?: ReactNode }) => (
+            <AppRouterStateContext.Provider value={state}>
+                {children}
+            </AppRouterStateContext.Provider>
+        ),
+        ...additionalProps
+    });
+}
+
+test("when modules are registered but not ready, and public data is ready, and protected data is ready, return true", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesRegistered = true;
+    state.areModulesReady = false;
+    state.isPublicDataReady = true;
+    state.isProtectedDataReady = true;
+
+    const { result } = renderUseCanRegisterDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeTruthy();
+});
+
+test("when public data is not ready but it's not required to wait for public data, return true", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesRegistered = true;
+    state.areModulesReady = false;
+    state.waitForPublicData = false;
+    state.isPublicDataReady = false;
+    state.isProtectedDataReady = true;
+
+    const { result } = renderUseCanRegisterDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeTruthy();
+});
+
+test("when protected data is not ready but it's not required to wait for protected data, return true", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesRegistered = true;
+    state.areModulesReady = false;
+    state.isPublicDataReady = true;
+    state.waitForProtectedData = false;
+    state.isActiveRouteProtected = true;
+    state.isProtectedDataReady = false;
+
+    const { result } = renderUseCanRegisterDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeTruthy();
+});
+
+test("when protected data is not ready but the route is public, return true", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesRegistered = true;
+    state.areModulesReady = false;
+    state.isPublicDataReady = true;
+    state.isActiveRouteProtected = false;
+    state.isProtectedDataReady = false;
+
+    const { result } = renderUseCanRegisterDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeTruthy();
+});
+
+test("when modules are ready, return false", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesRegistered = true;
+    state.areModulesReady = true;
+    state.isPublicDataReady = true;
+    state.isProtectedDataReady = true;
+
+    const { result } = renderUseCanRegisterDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeFalsy();
+});
+
+test("when the session is unauthorized, return false", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesRegistered = true;
+    state.areModulesReady = false;
+    state.isPublicDataReady = true;
+    state.isProtectedDataReady = true;
+    state.isUnauthorized = true;
+
+    const { result } = renderUseCanRegisterDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeFalsy();
+});
+
+test("when it's required to wait for public data and public data is not ready, return false", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesRegistered = true;
+    state.areModulesReady = false;
+    state.waitForPublicData = true;
+    state.isPublicDataReady = false;
+    state.isProtectedDataReady = true;
+
+    const { result } = renderUseCanRegisterDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeFalsy();
+});
+
+test("when it's required to wait for protected data and the protected data is not ready, return false", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesRegistered = true;
+    state.areModulesReady = false;
+    state.isPublicDataReady = true;
+    state.isActiveRouteProtected = true;
+    state.waitForProtectedData = true;
+    state.isProtectedDataReady = false;
+
+    const { result } = renderUseCanRegisterDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeFalsy();
+});
---
+++ packages/firefly/tests/useCanUpdateDeferredRegistrations.test.tsx
@@ -0,0 +1,99 @@
+import { renderHook, type RenderHookOptions } from "@testing-library/react";
+import type { ReactNode } from "react";
+import { AppRouterStateContext } from "../src/AppRouterContext.ts";
+import type { AppRouterState } from "../src/AppRouterReducer.ts";
+import { useCanUpdateDeferredRegistrations } from "../src/useCanUpdateDeferredRegistrations.ts";
+
+function createDefaultAppRouterState(): AppRouterState {
+    return {
+        areModulesReady: false,
+        areModulesRegistered: false,
+        isActiveRouteProtected: false,
+        isMswReady: false,
+        isProtectedDataReady: false,
+        isPublicDataReady: false,
+        isUnauthorized: false,
+        waitForMsw: false,
+        waitForProtectedData: false,
+        waitForPublicData: false
+    };
+}
+
+function renderUseCanUpdateDeferredRegistrationsHook<TProps>(state: AppRouterState, additionalProps: RenderHookOptions<TProps> = {}) {
+    return renderHook(() => useCanUpdateDeferredRegistrations(), {
+        wrapper: ({ children }: { children?: ReactNode }) => (
+            <AppRouterStateContext.Provider value={state}>
+                {children}
+            </AppRouterStateContext.Provider>
+        ),
+        ...additionalProps
+    });
+}
+
+test("when modules are ready, and the deferred registrations has been registered once, and the public data has been updated, return true", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesReady = true;
+    state.deferredRegistrationsUpdatedAt = Date.parse("2020-02-14");
+    state.publicDataUpdatedAt = Date.parse("2020-03-14");
+
+    const { result } = renderUseCanUpdateDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeTruthy();
+});
+
+test("when modules are ready, and the deferred registrations has been registered once, and the protected data has been updated, return true", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesReady = true;
+    state.deferredRegistrationsUpdatedAt = Date.parse("2020-02-14");
+    state.protectedDataUpdatedAt = Date.parse("2020-03-14");
+
+    const { result } = renderUseCanUpdateDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeTruthy();
+});
+
+test("when modules are ready, and the deferred registrations has been registered once, and the public data has been updated, and the protected data has been updated, return true", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesReady = true;
+    state.deferredRegistrationsUpdatedAt = Date.parse("2020-02-14");
+    state.publicDataUpdatedAt = Date.parse("2020-03-14");
+    state.protectedDataUpdatedAt = Date.parse("2020-03-14");
+
+    const { result } = renderUseCanUpdateDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeTruthy();
+});
+
+test("when modules are not ready, return false", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesReady = false;
+    state.deferredRegistrationsUpdatedAt = Date.parse("2020-02-14");
+    state.publicDataUpdatedAt = Date.parse("2020-03-14");
+
+    const { result } = renderUseCanUpdateDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeFalsy();
+});
+
+test("when there's no deferred registrations, return false", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesReady = true;
+    state.deferredRegistrationsUpdatedAt = undefined;
+    state.publicDataUpdatedAt = Date.parse("2020-03-14");
+
+    const { result } = renderUseCanUpdateDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeFalsy();
+});
+
+test("when the public and protected data has not been updated, return false", () => {
+    const state = createDefaultAppRouterState();
+    state.areModulesReady = true;
+    state.deferredRegistrationsUpdatedAt = Date.parse("2020-02-14");
+    state.publicDataUpdatedAt = undefined;
+    state.protectedDataUpdatedAt = undefined;
+
+    const { result } = renderUseCanUpdateDeferredRegistrationsHook(state);
+
+    expect(result.current).toBeFalsy();
+});

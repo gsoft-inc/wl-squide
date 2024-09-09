d46b6d7dc4121cda5540eb334eb824e593e32460
patricklafrance <pa.lafrance@gmail.com>
2024-09-09 11:33:55 -0400

    fix: useNavigationItems now accepts options

--- packages/react-router/src/useRuntimeNavigationItems.ts
+++ packages/react-router/src/useRuntimeNavigationItems.ts
@@ -1,13 +1,13 @@
 import { useRuntime } from "@squide/core";
 import type { ReactRouterRuntime } from "./reactRouterRuntime.ts";
 
-export interface UseNavigationItemsOptions {
+export interface UseRuntimeNavigationItemsOptions {
     menuId?: string;
 }
 
 // This hook has been renamed from useNavigationItems to useRuntimeNavigationItems to free up the name for the
 // use useNavigationItems hook of the @squide/firefly package.
-export function useRuntimeNavigationItems({ menuId }: UseNavigationItemsOptions = {}) {
+export function useRuntimeNavigationItems({ menuId }: UseRuntimeNavigationItemsOptions = {}) {
     const runtime = useRuntime() as ReactRouterRuntime;
 
     return runtime.getNavigationItems(menuId);

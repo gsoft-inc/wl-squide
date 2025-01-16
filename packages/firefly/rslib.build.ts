64516475ea7c8f14129354f01ed8f7921d9fef62
patricklafrance <pa.lafrance@gmail.com>
2025-01-16 14:27:01 -0500

    More stuff

--- packages/firefly/rslib.build.ts
+++ packages/firefly/rslib.build.ts
@@ -2,5 +2,6 @@ import { defineBuildConfig } from "@workleap/rslib-configs";
 import path from "node:path";
 
 export default defineBuildConfig({
+    react: true,
     tsconfigPath: path.resolve("./tsconfig.build.json")
 });
